a8359c521f9a578cf023a460e2dfd152018c953c
Petra <46897600+SDCGT@users.noreply.github.com>
2020-11-28 22:48:01 +0800

    ok

---
+++ door_animation/Assets/ScriptOnSward.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptOnSward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 pos;
+    public GameObject AnimatorOnIt;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "stone")
+        {
+            ContactPoint contact = collision.contacts[0];
+            pos = contact.point;
+            //Debug.Log("UnityIsShit");
+            AnimatorOnIt.SendMessage("Touch", pos);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "stone")
+        {
+            AnimatorOnIt.SendMessage("Leave");
+        }
+    }
+}
---
+++ door_animation/Assets/SriptOnAnimator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SriptOnAnimator : MonoBehaviour
+{
+    Vector3 touchPoint;
+    bool animatePlay;
+    bool touched;
+    public Animator m_animator;
+    public ParticleSystem fire;
+
+    private void Update()
+    {
+        if (animatePlay&touched)
+        {
+            PlayParticle(touchPoint);
+        }
+        
+    }
+
+    void Spark()
+    {
+        //Debug.Log("Spark!");
+        animatePlay = true;
+    }
+
+    void AttackEnd()
+    {
+        //Debug.Log("AttackEnd");
+        animatePlay = false;
+    }
+    
+    void Touch(Vector3 pos)
+    {
+        Debug.Log("Touch:"+pos);
+        touchPoint = pos;
+        touched = true;
+    }
+    void Leave()
+    {
+        Debug.Log("Leave");
+        touched = false;
+    }
+
+    void PlayParticle(Vector3 touch)
+    {
+        Debug.Log("PlayParticle");
+        fire.Play();
+    }
+}
